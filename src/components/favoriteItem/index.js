e0784afddb92483551f1f3a523f51409b232275c
Rufino Rojas <ruf_rm@hotmail.com>
2022-10-13 18:35:26 -0500

    filter favorites and delete from favorites

--- src/components/favoriteItem/index.js
+++ src/components/favoriteItem/index.js
@@ -2,16 +2,19 @@ import React, { useState } from 'react';
 import { Text, View, Pressable, Image } from 'react-native';
 import { styles } from './styles';
 
-const FavoriteItem = ({ title, price, icon, image, onPress }) => {
+const FavoriteItem = ({ title, price, icon, image, onPress, onIconPress }) => {
+    const BASE_URL_API = 'https://listicle.deegeehub.com/api';
 
     return (
         <Pressable onPress={onPress} style={styles.container}>
-            <Image style={styles.image} source={{ uri: image }} />
+            <Image style={styles.image} source={{ uri: `${BASE_URL_API}/${image?.path}` }} />
             <View style={styles.content}>
                 <Text style={styles.title}>{title}</Text>
-                <Text style={styles.price}>{price}</Text>
+                <Text style={styles.price}>${price}</Text>
             </View>
-            <Image source={icon || require('../../assets/close.png')} style={styles.icon} />
+            <Pressable onPress={onIconPress}>
+                <Image source={icon || require('../../assets/close.png')} style={styles.icon} />
+            </Pressable>
         </Pressable>
     )
 }
--- src/screens/app/Favorites/index.js
+++ src/screens/app/Favorites/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import { FlatList, View } from 'react-native';
+import React, { useContext } from 'react';
+import { FlatList, View, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
 import { products } from '../../../data/products'
 import FavoriteItem from '../../../components/favoriteItem';
 import Header from '../../../components/header';
+import { ServicesContext } from '../../../../App';
+import { updateService } from '../../../utils/backendCalls';
 
 const Favorites = ({ navigation }) => {
+
+    const { services, setServices } = useContext(ServicesContext);
+    const likedServices = Array.isArray(services) ? services?.filter(service => service?.liked) : [];
 
     const renderProducts = ({ item }) => {
         const onProductPress = () => {
             navigation.navigate('ProductDetails', { product: item })
         }
+
+        const onRemove = async () => {
+            const updateServices = await updateService(item?._id, { liked: false });
+            if (Array.isArray(updateServices)) {
+                setServices(updateServices);
+            }
+        }
+
+        const onIconPress = () => {
+            Alert.alert('Aye you sure you want to remove this item from you favorites?', '',
+                [{ text: 'Yes', onPress: onRemove }, { text: 'Cancel' }])
+        }
         return (
-            <FavoriteItem onPress={onProductPress} {...item} />
+            <FavoriteItem onPress={onProductPress} onIconPress={onIconPress} {...item} />
         )
     }
 
     return (
         <SafeAreaView style={styles.main}>
             <Header title="Favorites" />
-            <FlatList data={products} renderItem={renderProducts} keyExtractor={(item) => String(item?.id)} />
+            <FlatList
+                data={likedServices}
+                renderItem={renderProducts}
+                keyExtractor={services?._id}
+            />
         </SafeAreaView>
     )
 }
